ffd51a008eb2aeec77f63ce0dcfb22dc0be0a0e8
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-09 23:37:38 +0300

    connect to the database

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
 app.use(cors());
 
-app.listen(4000, () => {
-  console.log("the server is up!");
-});
+(async function () {
+  await mongoose.connect(process.env.MONGO_DB_URI);
+  app.listen(process.env.PORT, () => {
+    console.log("the server is up and listening on Port:", process.env.PORT);
+  });
+})();
