9f8105eb152eed43bae22aadc69c3a4cdbba5190
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-09 23:52:14 +0300

    create user model

---
+++ models/userModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    email: {
+        type: String, 
+        required: true,
+        unique: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    picture: {
+        type: String,
+        required: true,
+    },
+    
+}, { timestamps: true });
+module.exports = mongoose.model("User", userSchema);
