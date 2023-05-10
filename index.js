1d14b392e8cc0100c1c98dcc86e5df78061de162
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-10 03:04:19 +0300

    add body parser 'express.json'

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ const cors = require("cors");
 const userRouter = require("./routes/userRoutes");
 
 const app = express();
+app.use(express.json());
 app.use(cors());
-app.use(
-  "/user",
-  userRouter
-)(async function () {
-  await mongoose.connect(process.env.MONGO_DB_URI);
+app.use("/user", userRouter);
+
+mongoose.connect(process.env.MONGO_DB_URI).then(() => {
   app.listen(process.env.PORT, () => {
     console.log("the server is up and listening on Port:", process.env.PORT);
   });
-})();
+});
