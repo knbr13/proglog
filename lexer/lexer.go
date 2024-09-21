a8326f94305a1feedc7916995cd1c91f9000017f
knbr13 <abdullahalaadine63@gmail.com>
2024-09-21 23:58:24 +0300

    add isDigit func

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -81,3 +81,7 @@ func (l *Lexer) readIdentifier() string {
 func isLetter(ch byte) bool {
 	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
