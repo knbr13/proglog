0bc333a05ac8112c6a2a68bccbe4ba86c6d1ccb3
knbr13 <abdullahalaadine63@gmail.com>
2025-06-27 16:58:30 +0300

    add Append receiver func to the Log type

--- internal/log/log.go
+++ internal/log/log.go
@@ -7,6 +7,8 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+
+	api "github.com/knbr13/proglog/api/v1"
 )
 
 type Log struct {
@@ -64,4 +66,19 @@ func (l *Log) setup() error {
 		}
 	}
 	return nil
+}
+
+func (l *Log) Append(record *api.Record) (uint64, error) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	off, err := l.activeSegment.Append(record)
+	if err != nil {
+		return 0, err
+	}
+
+	if l.activeSegment.IsMaxed() {
+		err = l.newSegment(off + 1)
+	}
+	return off, err
 }
