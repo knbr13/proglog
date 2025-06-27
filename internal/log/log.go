5cd16ff8ea3744e2d19e754a632756016407f5c3
knbr13 <abdullahalaadine63@gmail.com>
2025-06-27 16:55:26 +0300

    add a setup func for the Log type

--- internal/log/log.go
+++ internal/log/log.go
@@ -1,6 +1,11 @@
 package log
 
 import (
+	"os"
+	"path"
+	"sort"
+	"strconv"
+	"strings"
 	"sync"
 )
 
@@ -24,4 +29,39 @@ func NewLog(dir string, c Config) (*Log, error) {
 		Config: c,
 	}
 	return l, l.setup()
+}
+
+func (l *Log) setup() error {
+	files, err := os.ReadDir(l.Dir)
+	if err != nil {
+		return err
+	}
+	var baseOffsets []uint64
+	for _, file := range files {
+		offStr := strings.TrimSuffix(
+			file.Name(),
+			path.Ext(file.Name()),
+		)
+		off, _ := strconv.ParseUint(offStr, 10, 0)
+		baseOffsets = append(baseOffsets, off)
+	}
+	sort.Slice(baseOffsets, func(i, j int) bool {
+		return baseOffsets[i] < baseOffsets[j]
+	})
+	for i := 0; i < len(baseOffsets); i++ {
+		if err = l.newSegment(baseOffsets[i]); err != nil {
+			return err
+		}
+		// baseOffset contains dup for index and store so we skip
+		// the dup
+		i++
+	}
+	if l.segments == nil {
+		if err = l.newSegment(
+			l.Config.Segment.InitialOffset,
+		); err != nil {
+			return err
+		}
+	}
+	return nil
 }
