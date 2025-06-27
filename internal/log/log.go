cd974be83a6fb0749f8bfc97245747363e840f38
knbr13 <abdullahalaadine63@gmail.com>
2025-06-27 16:54:59 +0300

    create a NewLog func

--- internal/log/log.go
+++ internal/log/log.go
@@ -1,6 +1,8 @@
 package log
 
-import "sync"
+import (
+	"sync"
+)
 
 type Log struct {
 	mu            sync.RWMutex
@@ -8,4 +10,18 @@ type Log struct {
 	Config        Config
 	activeSegment *segment
 	segments      []*segment
+}
+
+func NewLog(dir string, c Config) (*Log, error) {
+	if c.Segment.MaxStoreBytes == 0 {
+		c.Segment.MaxStoreBytes = 1024
+	}
+	if c.Segment.MaxIndexBytes == 0 {
+		c.Segment.MaxIndexBytes = 1024
+	}
+	l := &Log{
+		Dir:    dir,
+		Config: c,
+	}
+	return l, l.setup()
 }
