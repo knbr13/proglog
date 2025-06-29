ee3efc39764ce87616f0241f27ddadca53ff2835
knbr13 <abdullahalaadine63@gmail.com>
2025-06-30 00:33:40 +0300

    use the new http pkg

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"log"
 
+	logpkg "github.com/knbr13/proglog/internal/log"
 	"github.com/knbr13/proglog/internal/server"
 )
 
 func main() {
-	srv := server.NewHTTPServer(":8080")
+	logInstance, err := logpkg.NewLog("./data", logpkg.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	srv := server.NewHTTPServer(":8080", logInstance)
 	log.Fatal(srv.ListenAndServe())
 }
